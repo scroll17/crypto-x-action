9371dfe7207df26228f71232017f964de59068ac
Denys Zolotarenko <zololotarenko.2015@gmail.com>
2023-11-12 20:45:42 +0200

    server(database/schemas/blockchain-network): add static methods

--- server/src/database/schemas/blockchain-network/blockchain-network.schema.ts
+++ server/src/database/schemas/blockchain-network/blockchain-network.schema.ts
@@ -1,7 +1,11 @@
+import _ from 'lodash';
 import { Type } from '@nestjs/common';
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { HydratedDocument, Model, Schema as MongooseSchema } from 'mongoose';
+import { FilterQuery, HydratedDocument, Model, Schema as MongooseSchema } from 'mongoose';
 import { BlockchainNetworks } from '@common/blockchain/enums';
+import { PaginateResultEntity } from '@common/entities';
+import { FindBlockchainNetworkDto } from '../../../modules/dbl/blockchain/network/dto';
+import { BlockchainNetworkEntity } from './blockchain-network.entity';
 
 export type BlockchainNetworkDocument = HydratedDocument<BlockchainNetwork> & TStaticMethods;
 export type BlockchainNetworkModel = Model<BlockchainNetworkDocument> & TStaticMethods;
@@ -29,6 +33,48 @@ export const BlockchainNetworkSchema = SchemaFactory.createForClass(
 // INDEXES
 
 // CUSTOM TYPES
-type TStaticMethods = {};
+type TStaticMethods = {
+  paginate: (
+    this: BlockchainNetworkModel,
+    findOptions: FindBlockchainNetworkDto,
+  ) => Promise<PaginateResultEntity<BlockchainNetworkEntity>>;
+};
 
 // STATIC METHODS IMPLEMENTATION
+BlockchainNetworkSchema.statics.paginate = async function (
+  findOptions,
+): Promise<PaginateResultEntity<BlockchainNetworkEntity>> {
+  const { filter: rawFilter, paginate, sort } = findOptions;
+  const { count, page } = paginate;
+
+  const filter = Object.fromEntries(
+    Object.entries(_.omit({ ...rawFilter }, ['id'])).filter(([, value]) => !_.isNil(value)),
+  );
+
+  const skip = (page - 1) * count;
+  const where: FilterQuery<BlockchainNetworkDocument> = rawFilter?.id
+    ? { _id: rawFilter.id, ...filter }
+    : { ...filter };
+
+  if ('name' in where && where.name) {
+    // where.name = where.name
+  }
+
+  const total = await this.count(where);
+  const data = (await this.aggregate()
+    .match(where)
+    .skip(skip)
+    .limit(count)
+    .sort(sort ? { [sort.name]: sort.type } : { _id: 'desc' })
+    .exec()) as BlockchainNetworkEntity[];
+
+  return {
+    data: data,
+    meta: {
+      total,
+      page,
+      count,
+      lastPage: Math.ceil(total / count),
+    },
+  };
+} as TStaticMethods['paginate'];
