af6eb1984b18810f420f6bbdd846b8e6ce5187b5
Denys Zolotarenko <zololotarenko.2015@gmail.com>
2024-01-03 17:27:30 +0200

    server(common/enums/rest): add HTTP protocol enum

---
+++ server/src/common/enums/rest/http-protocol.enum.ts
@@ -0,0 +1,4 @@
+export enum HttpProtocol {
+  HTTP = 'http',
+  HTTPS = 'https',
+}
--- server/src/common/enums/rest/index.ts
+++ server/src/common/enums/rest/index.ts
@@ -1,2 +1,3 @@
 export { EditAction } from './edit-action.enum';
 export { SortType } from './sort-type.enum';
+export { HttpProtocol } from './http-protocol.enum';
