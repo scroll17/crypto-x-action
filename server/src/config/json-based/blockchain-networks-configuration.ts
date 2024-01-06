1b67d7bbcb799b9574638afb160e12339793dba8
Denys Zolotarenko <zololotarenko.2015@gmail.com>
2024-01-06 20:33:51 +0200

    server(config): add blockchain-networks configuration loader

---
+++ server/src/config/json-based/blockchain-networks-configuration.ts
@@ -0,0 +1,19 @@
+import * as path from 'node:path';
+import * as fs from 'node:fs';
+import { IBlockchainNetworkEnvConfig } from '@common/blockchain/types/env-config';
+import { AppConstants } from '../../app.constants';
+
+export const blockchainNetworksConfiguration = (): {
+  blockchainNetworks: Array<IBlockchainNetworkEnvConfig>;
+} => {
+  const networksFilePath = path.resolve(__dirname, '../../../', AppConstants.Env.BlockchainNetworksFileName);
+  fs.accessSync(networksFilePath, fs.constants.R_OK | fs.constants.W_OK);
+
+  const networks = JSON.parse(
+    fs.readFileSync(networksFilePath, {
+      encoding: 'utf8',
+    }),
+  );
+
+  return { blockchainNetworks: networks };
+};
