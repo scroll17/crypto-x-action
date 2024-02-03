682dedfea6ec12e104cadaa468298b32eff26663
Denys Zolotarenko <zololotarenko.2015@gmail.com>
2024-02-03 19:59:19 +0200

    server(modules/integrations/crypto-compare): connect Integration records logic

--- server/src/modules/integrations/crypto-compare/crypto-compare.module.ts
+++ server/src/modules/integrations/crypto-compare/crypto-compare.module.ts
@@ -2,12 +2,20 @@ import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
 import { CryptoCompareService } from './crypto-compare.service';
 import { CryptoCompareController } from './crypto-compare.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Integration, IntegrationSchema } from '@schemas/integration';
 
 @Module({
   imports: [
     HttpModule.register({
       maxRedirects: 5,
     }),
+    MongooseModule.forFeature([
+      {
+        name: Integration.name,
+        schema: IntegrationSchema,
+      },
+    ]),
   ],
   providers: [CryptoCompareService],
   controllers: [CryptoCompareController],
--- server/src/modules/integrations/crypto-compare/crypto-compare.service.ts
+++ server/src/modules/integrations/crypto-compare/crypto-compare.service.ts
@@ -1,11 +1,9 @@
-import {HttpException, HttpStatus, Injectable, Logger, OnModuleInit} from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 import { firstValueFrom } from 'rxjs';
-import {IntegrationNames} from "@common/integrations/common";
-
-const DEBANK_HOST = 'https://api.debank.com';
-
-const HOST = 'https://min-api.cryptocompare.com';
+import { IntegrationNames } from '@common/integrations/common';
+import { Integration, IntegrationDocument, IntegrationModel } from '@schemas/integration';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class CryptoCompareService implements OnModuleInit {
@@ -13,17 +11,41 @@ export class CryptoCompareService implements OnModuleInit {
 
   private readonly logger = new Logger(this.constructor.name);
 
-  private integrationRec: '';
-
+  private apiUrl: string;
+  private integration: IntegrationDocument;
   private readonly cachedPrice: Map<string, Record<string, number>> = new Map();
 
-  constructor(private readonly httpService: HttpService) {}
+  constructor(
+    private readonly httpService: HttpService,
+    @InjectModel(Integration.name) private readonly integrationModel: IntegrationModel,
+  ) {}
 
   public async onModuleInit() {
-    this.logger.verbose(`Load integration record by key: "${this.INTEGRATION_KEY}"`, { key: this.INTEGRATION_KEY });
+    this.logger.verbose(`Load integration record by key: "${this.INTEGRATION_KEY}"`, {
+      key: this.INTEGRATION_KEY,
+    });
 
-    await this.getPrices('ETH', ['USD', 'EUR']);
-    await this.getPrices('ETH', ['BTC']);
+    const integration = await this.integrationModel.findOne({ key: this.INTEGRATION_KEY }).exec();
+    if (!integration) {
+      throw new HttpException(`Integration "${this.INTEGRATION_KEY}" not found`, HttpStatus.NOT_FOUND);
+    }
+
+    this.apiUrl = integration.apiUrl;
+    this.integration = integration;
+    this.logger.debug(`Integration "${this.INTEGRATION_KEY}" loaded:`, {
+      key: integration.key,
+      active: integration.active,
+      apiUrl: integration.apiUrl,
+    });
+
+    // await this.getPrices('ETH', ['USD', 'EUR']);
+    // await this.getPrices('ETH', ['BTC']);
+  }
+
+  private checkActiveStatus() {
+    if (!this.integration.active) {
+      throw new HttpException(`Integration "${this.INTEGRATION_KEY}" is not active`, HttpStatus.BAD_REQUEST);
+    }
   }
 
   private isErrorResponse(data: Record<string, unknown>) {
@@ -31,11 +53,11 @@ export class CryptoCompareService implements OnModuleInit {
     //     Message: 'Path does not exist',
   }
 
-  private getCachedPrice(fromSymbol: string, toSymbols: string[]) {
-
-  }
+  private getCachedPrice(fromSymbol: string, toSymbols: string[]) {}
 
   public async getPrices(fromSymbol: string, toSymbols: string[], cache = true) {
+    this.checkActiveStatus();
+
     if (toSymbols.length === 0) {
       throw new HttpException('Passed invalid "toSymbols"', HttpStatus.BAD_REQUEST);
     }
@@ -43,7 +65,7 @@ export class CryptoCompareService implements OnModuleInit {
     const query = new URLSearchParams(toSymbols.map((s) => ['tsyms', s]));
     query.append('fsym', fromSymbol);
 
-    const url = `${HOST}/data/price?${query.toString()}`;
+    const url = `${this.apiUrl}/data/price?${query.toString()}`;
 
     try {
       const { data } = await firstValueFrom(this.httpService.get<Record<string, number>>(url));
@@ -58,7 +80,7 @@ export class CryptoCompareService implements OnModuleInit {
     }
   }
 
-  public async getPrice(fromSymbol: string, toSymbol: string, cache = true) {
+  public async getPrice(fromSymbol: string, toSymbol: string, cache = true) {}
 
-  }
+  public getRateLimit() {}
 }
