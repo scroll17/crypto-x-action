40bda30d91dc20c1f396554200ff0bf7f963bb63
Denys Zolotarenko <zololotarenko.2015@gmail.com>
2024-02-16 20:41:31 +0200

    server(common/integrations/scroll-block-scout/types): extend types

--- server/src/common/integrations/scroll-block-scout/types/responses/index.ts
+++ server/src/common/integrations/scroll-block-scout/types/responses/index.ts
@@ -2,3 +2,7 @@ export { IScrollBlockScoutGenericResponse } from './generic.interface';
 export { TScrollBlockScoutTotalFeesResponse, IScrollBlockScoutTotalFeesData } from './total-fees.interface';
 export { TScrollBlockScoutCoinPriceResponse, IScrollBlockScoutCoinPriceData } from './coin-price.interface';
 export { TScrollBlockScoutAccountBalanceResponse } from './account-balance.interface';
+export {
+  TScrollBlockScoutMultiAccountBalanceResponse,
+  IScrollBlockScoutTotalMultiAccountBalanceData,
+} from './multi-account-balance.interface';
