4b9030f1e0f98a8e273484b0cf124c4865d6f02a
Denys Zolotarenko <zololotarenko.2015@gmail.com>
2024-02-17 16:14:38 +0200

    server(env): add linea-explorer integration

--- server/src/common/integrations/common/enums/integration-names.enum.ts
+++ server/src/common/integrations/common/enums/integration-names.enum.ts
@@ -2,4 +2,5 @@ export enum IntegrationNames {
   CryptoCompare = 'crypto-compare',
   BaseBlockScout = 'base-block-scout',
   ScrollBlockScout = 'scroll-block-scout',
+  LineaExplorer = 'linea-explorer',
 }
