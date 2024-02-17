4fac69ab0f5c78288a2a0153893ad4045064fe39
Denys Zolotarenko <zololotarenko.2015@gmail.com>
2024-02-17 16:13:53 +0200

    server(constants): add Integration block

--- server/src/app.constants.ts
+++ server/src/app.constants.ts
@@ -13,4 +13,26 @@ export namespace AppConstants {
     export const IntegrationsFileName = '.integrations.json';
     export const IntegrationsFilePath = path.resolve(EnvFolderPath, IntegrationsFileName);
   }
+
+  export namespace Integration {
+    export namespace Scroll {
+      export const COIN_CONTRACTS = {
+        USDC: {
+          token: 'USDC',
+          address: '0x06eFdBFf2a14a7c8E15944D1F4A48F9F95F663A4',
+          decimals: 6,
+        },
+        USDT: {
+          token: 'USDT',
+          address: '0xf55BEC9cafDbE8730f096Aa55dad6D22d44099Df',
+          decimals: 6,
+        },
+        DAI: {
+          token: 'DAI',
+          address: '0xcA77eB3fEFe3725Dc33bccB54eDEFc3D9f764f97',
+          decimals: 18,
+        },
+      } as const;
+    }
+  }
 }
